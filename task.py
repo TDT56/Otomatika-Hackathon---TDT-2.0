52fd59af7569dcd90f85188b17e6c1f17283710c
Tian <tianexternal@gmail.com>
2022-03-11 19:48:03 +0200

    attempt to click USA link

--- task.py
+++ task.py
@@ -3,16 +3,18 @@
 
 from RPA.Browser.Selenium import Selenium
 
-browser_lib = Selenium()
+browser = Selenium()
 
 def open_the_website(url):
-    browser_lib.open_available_browser(url)
+
+    browser.open_available_browser(url)
+    browser.click_link('USA')
 
 def main():
     try:
         open_the_website("https://www.worldometers.info/coronavirus/")
     finally:
-        pass    # browser_lib.close_all_browsers()
+        browser.close_all_browsers()
 
 if __name__ == "__main__":
     main()
