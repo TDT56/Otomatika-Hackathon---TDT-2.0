a5e1541d3183175b7bdc638967f6e2fc749cf2c9
Tian <tianexternal@gmail.com>
2022-03-12 12:04:18 +0200

    new website chosen, click on link, take screenshot

--- task.py
+++ task.py
@@ -6,15 +6,20 @@ from RPA.Browser.Selenium import Selenium
 browser = Selenium()
 
 def open_worldometers_website():
-    browser.open_chrome_browser("https://www.worldometers.info/coronavirus/")
+    browser.open_available_browser("https://covid19.who.int/")
 
-def click_USA():
-    browser.click_link('USA')
+def click_Data_Table():
+    browser.click_link('Data Table')
 
 def main():
     try:
         open_worldometers_website()
-        click_USA()
+        click_Data_Table()
+        browser.wait_until_page_contains('Situation by Region, Country, Territory & Area')
+        browser.capture_page_screenshot('Screenshot2.png')
+        #browser.capture_page_screenshot
+        #USA_table_contents = browser.get_element_attribute('usa_table_countries_today', 'outerHTML')
+        #print(USA_table_contents)
     finally:
         browser.close_all_browsers()
 
